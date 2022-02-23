7ca71915b842b6a534cb3151fe3d2e10b5661cdf
vadimm <vadimm@saltedge.com>
2022-02-23 18:35:16 +0200

    Updated SpringBoot for BG, also modified RestTemplate

---
+++ saltedge-compliance-connector-java-v2/compliance-connector-sdk/src/main/java/com/saltedge/connector/sdk/config/RestTemplateConfiguration.java
@@ -0,0 +1,20 @@
+package com.saltedge.connector.sdk.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+    }
+}
